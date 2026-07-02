1bec1b56d9b1b97593a6fed4f0256ef411d8b2a0
Guerrero <matias.guerrero@accenture.com>
2026-07-02 13:05:59 -0300

    seccion 3 - Variables y constantes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+const firstName: string = "Matias"
+let lastName ="Guerrero";
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterH = lastName.includes('h');
+
+console.log({containsLetterH, diceNumber});
