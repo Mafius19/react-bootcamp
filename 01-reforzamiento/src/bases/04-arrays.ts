69d3541727ccd5535eb979bcbbac64a9b4c04508
Guerrero <matias.guerrero@accenture.com>
2026-07-03 11:25:44 -0300

    seccion 3 - Arreglos

---
+++ 01-reforzamiento/src/bases/04-arrays.ts
@@ -0,0 +1,12 @@
+const myArray:number[] =[1,2,3,4,5,6]
+const myArray2 = [...myArray]
+// myArray.push(10);
+// myArray.push(11);
+
+// console.log(myArray);
+
+// for (const myNumber of myArray) {
+//     console.log(myNumber + 10)
+// } 
+myArray2.push(7)
+console.log({myArray, myArray2})
