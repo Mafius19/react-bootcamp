c1c772bb06d8f80caf9dea97710a52685ee8aacb
Guerrero <matias.guerrero@accenture.com>
2026-07-07 16:57:38 -0300

    seccion 3 - Interfaces y Optimización

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,19 +1,23 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
+
 const API_KEY = 'VkixtOePYSMYaEzZywX6tL26yz7xNhDG';
 
 const myRequest = fetch(
     `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
 );
 
-myRequest
-    .then(response => response.json())
-    .then(data => {
-        const imageUrl = data.data.images.original.url;
-        console.log(imageUrl);
-
-        const imgElement = document.createElement('img');
-        imgElement.src = imageUrl;
+const createImageInsideDOM = (url:string) => {
+    const imgElement = document.createElement('img');
+        imgElement.src = url;
 
         document.body.append(imgElement);
+}
+
+myRequest
+    .then(response => response.json())
+    .then(({data}: GiphyRandomResponse) => {
+        const imageUrl = data.images.original.url;
+        createImageInsideDOM(imageUrl)
     })
     .catch( err => {
         console.error(err)
