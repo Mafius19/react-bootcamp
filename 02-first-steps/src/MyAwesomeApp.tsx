2bb770f890026f670da7d2cdd4fd4456cd87d4b2
Guerrero <matias.guerrero@accenture.com>
2026-07-12 18:52:14 -0300

    Seccion 4 - Impresion de variables

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,9 +1,26 @@
+const firstName = 'Matias';
+const lastName = 'Guerrero'
+
+const favoriteGames = ['Elden Ring', 'Smash', 'Metal Gear'];
+const isActive = true
+
+const addres= {
+  zipCode: 'ABC-123',
+  country: 'Canadá'
+}
 
 export const MyAwesomeApp = () => {
+
+
   return (
     <>
-      <h1>Matias</h1>
-      <h3>Guerrero</h3>
+      <h1>{firstName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favoriteGames.join(', ')}</p>
+      <h1>{isActive ? 'Activo' : 'No activo'} </h1>
+
+      <p>{JSON.stringify(addres)}</p>
     </>
   )
 }
