7570e1b33786ee4e9579edabf11c94745eb2627e
Guerrero <matias.guerrero@accenture.com>
2026-07-12 19:58:30 -0300

    Seccion 4 - Mostrar listado de elementos

--- 02-first-steps/src/FirstStpesApp.tsx
+++ 02-first-steps/src/FirstStpesApp.tsx
@@ -1,14 +1,28 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+  productName: string;
+  quantity: number;
+}
+
+const ItemsInCart : ItemInCart[] = [
+  {productName: 'Nintendo Switch 2', quantity: 1},
+  {productName: 'Pro Controlloer', quantity: 2},
+  {productName: 'Super Smash', quantity: 5},
+]
 export function FirstStepsApp() {
   return (
     <>
       <h1>Carrito de compras</h1>
 
-      <ItemCounter name='Nintendo Switch 2' quantity={1}/>
+      {/* <ItemCounter name='Nintendo Switch 2' quantity={1}/>
       <ItemCounter name='Pro Controller' quantity={2}/>
       <ItemCounter name='Super Smash' quantity={3}/>
-      <ItemCounter name='Super Mario' quantity={3}/>
+      <ItemCounter name='Super Mario' quantity={3}/> */}
+      {ItemsInCart.map(({productName, quantity}) => (
+          <ItemCounter key={productName} name={productName} quantity={quantity}/>
+        ))
+      }
     </>
   )
 }
