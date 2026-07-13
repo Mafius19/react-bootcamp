befeb9fab4a6e28eb70ce292e5e6f1e428cddc1e
Guerrero <matias.guerrero@accenture.com>
2026-07-13 19:24:20 -0300

    Seccion 5 - Evaluar snapshots

--- 02-first-steps/src/MyAwesomeApp.test.tsx
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -24,4 +24,14 @@ describe('MyAwesomeApp', () => {
     const h1 = screen.getByTestId('first-name-title');
     expect(h1.innerHTML).toContain('Matias');
   })
+
+  test('should match snapshot', () => {
+    const {container } = render(<MyAwesomeApp />)
+    expect(container).toMatchSnapshot();
+  })
+
+  test('should match snapshot', () => {
+    render(<MyAwesomeApp />)
+    expect(screen.findAllByTestId('div-app')).toMatchSnapshot();
+  })
 })
