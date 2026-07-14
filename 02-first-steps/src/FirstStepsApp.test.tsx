7f2a91ff5b3da19b503559b5852fcab02b1109e9
Guerrero <matias.guerrero@accenture.com>
2026-07-13 22:16:51 -0300

    Seccion 5 - Componentes ficticios - Mock Components

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,7 +1,11 @@
-import { render } from "@testing-library/react"
-import { describe, expect, test } from "vitest"
+import { render, screen } from "@testing-library/react"
+import { describe, expect, test, vi } from "vitest"
 import { FirstStepsApp } from "./FirstStepsApp"
 
+vi.mock('./shopping-cart/ItemCounter', () => ({
+  ItemCounter: () => <div data-testid="ItemCounter"/>
+  
+}));;
 
 describe('FirstStepsApp', () => {
 
@@ -9,5 +13,15 @@ describe('FirstStepsApp', () => {
     const { container } = render(<FirstStepsApp />)
     expect(container).toMatchSnapshot();
 
+  });
+
+  test('should render the correct number of ItemCounter components', () => {
+    render(<FirstStepsApp />)
+
+    const itemCounters = screen.getAllByTestId('ItemCounter');
+    expect(itemCounters.length).toBe(3); // Assuming there are 3 items in the ItemsInCart array
+    
+    
+    // screen.debug();
   });
 });
