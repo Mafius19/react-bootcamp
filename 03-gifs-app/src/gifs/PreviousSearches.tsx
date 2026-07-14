475e695401dccf9d3ce0bd3a882e8c54f72157c2
Guerrero <matias.guerrero@accenture.com>
2026-07-14 17:15:17 -0300

    Seccion 6 - Tarea GifList

---
+++ 03-gifs-app/src/gifs/PreviousSearches.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props {
+  searches: string[];
+}
+export const PreviousSearches = ({ searches }: Props) => {
+  return (
+    <div className="previous-searches">
+      <h2>Busquedas previas</h2>
+      <ul className="previous-searches-list">
+        {searches?.map((search) => (
+          <li key={search}>{search}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
