9f8a4d32dddef32eeadb89d797ec9ef97889680c
Guerrero <matias.guerrero@accenture.com>
2026-07-17 20:36:19 -0300

    Seccion 9 - useEffect - Disparar efectos secundarios

--- 04-hooks-app/src/02-useEffect/TrafficLightWithEffect.tsx
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithEffect.tsx
@@ -1,29 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const colors = {
   red: "bg-red-500 animate-pulse",
   yellow: "bg-yellow-500 animate-pulse",
   green: "bg-green-500 animate-pulse",
-  blue: "bg-blue-500 animate-pulse",
 
 }
 
-// type trafficLightColors = 'red' | 'yellow' | "green";
 type trafficLightColors = keyof typeof colors;
 
-export const TrafficLight = () => {
+export const TrafficLightWithEffect = () => {
 
   const [light, setLight] = useState<trafficLightColors>('red')
+  const [countdown, setCountdown] = useState(5)
+  useEffect(() => {
+    if (countdown === 0) return;
 
-  const handleColorChange = (color: trafficLightColors) => {
-      setLight((prev) => {
-      console.log({prev})
-      return color
-    })
-  }
+    const intervalId = setInterval(() => {
+      setCountdown(prev => prev -1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [countdown])
+  
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
       <div className="flex flex-col items-center space-y-8">
+        <h1 className="text-white text-3xl font-thin">Semáforo con useEffect</h1>
+        <h2 className="text-white text-xl">Countdown: {countdown}</h2>
         <div 
           className={`w-32 h-32 ${
             light === 'red' ? colors[light] : 'bg-gray-500'
@@ -44,27 +50,6 @@ export const TrafficLight = () => {
         {/* <div className="w-32 h-32 bg-yellow-500 rounded-full"></div>
         <div className="w-32 h-32 bg-green-500 rounded-full"></div> */}
 
-        {/* Botón para cambiar el estado de la luz */}
-        <div className="flex gap-2">
-          <button
-            className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer"
-            onClick={() => handleColorChange('red')}
-          >
-            Rojo
-          </button>
-          <button
-            className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer"
-            onClick={() => handleColorChange('yellow')}
-          >
-            Amarillo
-          </button>
-          <button
-            className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer"
-            onClick={() => handleColorChange('green')}
-          >
-            Verde
-          </button>
-        </div>
       </div>
     </div>
   );
