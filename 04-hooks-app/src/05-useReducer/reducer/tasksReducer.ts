14472ae422df8cba3d378bda486370aed68bd980
Guerrero <matias.guerrero@accenture.com>
2026-07-18 23:45:25 -0300

    Seccion 10 - Configuracion de la función reducer

--- 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
@@ -11,13 +11,51 @@ interface TaskState {
   pending: number;
 }
 
-export type TaskAction = 
-| { type: 'ADD_TODO', payload: string}
-| { type: 'TOGGLE', payload: number}
-| { type: 'DELETE_TODO', payload: number};
+export type TaskAction =
+  | { type: 'ADD_TODO', payload: string }
+  | { type: 'TOGGLE_TODO', payload: number }
+  | { type: 'DELETE_TODO', payload: number };
 
-export const tasksReducer = (state: TaskState, action: TaskAction):TaskState => {
+export const tasksReducer = (state: TaskState, action: TaskAction): TaskState => {
 
+  switch (action.type) {
+    case 'ADD_TODO': {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false
+      };
 
-  return state;
+      // ! No lo deben hacer
+      // state.todos.push(newTodo);
+
+      return {
+        ...state,
+        todos: [...state.todos, newTodo]
+      };
+    }
+
+    case 'DELETE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload)
+      };
+
+    case 'TOGGLE_TODO': {
+      const updateTodos = state.todos.map(todo => {
+        if (todo.id === action.payload) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+
+      return {
+        ...state,
+        todos: updateTodos
+      };
+    }
+
+    default:
+      return state;
+  }
 }
