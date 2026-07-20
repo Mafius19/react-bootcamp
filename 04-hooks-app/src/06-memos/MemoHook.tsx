67a7fde79858b14291c20ace7afa743e363982f2
Guerrero <matias.guerrero@accenture.com>
2026-07-20 20:02:16 -0300

    Seccion 11 - useCallBack - Memorizacion de funciones

--- 04-hooks-app/src/06-memos/MemoHook.tsx
+++ 04-hooks-app/src/06-memos/MemoHook.tsx
@@ -1,18 +1,28 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { MyTitle } from "./ui/MyTitle"
 import { MySubTitle } from "./ui/MySubTitle";
+
+
+// const handleMyApiCall = (myValue: string) => {
+//     console.log('Llamar a mi Api - ', myValue)
+//   }
+
 
 export const MemoHook = () => {
 
   const [title, setTitle] = useState('Hola');
   const [subTitle, setSubTitle] = useState('Mundo');
 
+  const handleMyApiCall = useCallback(() => {
+    console.log('Llamar a mi Api - ', subTitle)
+  }, [subTitle])
+
   return (
     <div className='bg-gradient flex flex-col gap-4'>
       <h1 className="text-2xl font-thin text-white">MemoApp</h1>
 
       <MyTitle title={title}/>
-      <MySubTitle subtitle={subTitle}/>
+      <MySubTitle subtitle={subTitle} callMyApi={handleMyApiCall}/>
 
 
       <button 
--- 04-hooks-app/src/06-memos/ui/MySubTitle.tsx
+++ 04-hooks-app/src/06-memos/ui/MySubTitle.tsx
@@ -2,15 +2,21 @@ import { memo } from "react";
 
 interface Props {
   subtitle: string;
+
+  // callMyApi: (value: string) => void;
+  callMyApi: () => void;
 }
-export const MySubTitle = memo(({subtitle}: Props) => {
+export const MySubTitle = memo(({subtitle, callMyApi }: Props) => {
 
   console.log('MySubTitle re-render')
   return (
     <>
       <h6 className="text-2xl font-bold">{subtitle}</h6>
 
-      <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer">
+      <button 
+        className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer"
+        onClick={callMyApi}  
+      >
         Llamar a función
       </button>
     </>
