a7750144697bf9b63d198818163397dc43447d4d
Guerrero <matias.guerrero@accenture.com>
2026-07-20 20:40:05 -0300

    Seccion 11 - useMemo memorizacion de valores

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -8,7 +8,8 @@ import { createRoot } from 'react-dom/client'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { TasksApp } from './05-useReducer/TaskApp'
 // import { ScrambleWords } from './05-useReducer/ScrambleWords'
-import { MemoHook } from './06-memos/MemoHook'
+// import { MemoHook } from './06-memos/MemoHook'
+import { MemoCounter } from './06-memos/MemoCounter'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <FocusScreen/> */}
     {/* <TasksApp/> */}
     {/* <ScrambleWords/> */}
-    <MemoHook/>
+    {/* <MemoHook/> */}
+    <MemoCounter/>
   </StrictMode>,
 )
