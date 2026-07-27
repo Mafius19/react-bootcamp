c13b8a1e0999648f34191b26ba573a8ec763b580
Matias Guerrero <matias.f.guerrero19@gmail.com>
2026-07-27 17:05:46 -0300

    Seccion 12 - Navegar entre pantallas - Diseño de paginas

--- 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Link } from 'react-router'
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='flex flex-col min-h-screen items-center'>
+      <h1 className='text-4xl font-bold'>Iniciar Sesión</h1>
+      <hr />
+
+      <form className='flex flex-col gap-2 my-10'>
+        <Input
+          type="number"
+          placeholder='ID del usuario'
+        />
+        <Button type='submit'>Login</Button>
+      </form>
+
+      <Link to="/about">
+        <Button variant="ghost">
+           Volver a la página principal
+        </Button>
+      </Link>
+    </div>
   )
 }
--- 04-hooks-app/src/09-useContext/pages/profile/ProfilePage.tsx
+++ 04-hooks-app/src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
 
 export const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h1 className="text-4xl">Perfil del usuario</h1>
+      <hr />
+
+      <pre className="my-4">{JSON.stringify({}, null, 2)}</pre>
+
+      <Button variant="destructive">Salir</Button>
+    </div>
   )
 }
