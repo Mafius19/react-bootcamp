a991822a8fb5eeff3a0cc5a46b951761cf8f03e3
Matias Guerrero <matias.f.guerrero19@gmail.com>
2026-07-29 18:33:17 -0300

    Seccion 13 - Componentes de busqueda y grid de personajes

--- 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -1,12 +1,17 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 export const SearchPage = () => {
   return (
     <>
       <CustomJumbotron title="Búsqueda de Superhéroes" description="Descubre, explora y administra super héroes y villanos" />
+      
       {/* Stats Dashboard */}
       <HeroStats/>
+
+        {/* Controls */}
+      <SearchControls/>
     </>
   )
 }
