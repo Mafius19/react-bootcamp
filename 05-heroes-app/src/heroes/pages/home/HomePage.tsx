ff1717c18120ad02ae5b561e779bde7540a2174e
Matias Guerrero <matias.f.guerrero19@gmail.com>
2026-07-30 19:53:27 -0300

    Seccion 14 - Tanstack Query y proyecto backend agregado

--- 05-heroes-app/src/heroes/pages/home/HomePage.tsx
+++ 05-heroes-app/src/heroes/pages/home/HomePage.tsx
@@ -6,17 +6,22 @@ import { HeroGrid } from "@/heroes/components/HeroGrid"
 import { useEffect, useState } from "react"
 import { CustomPagination } from "@/components/custom/CustomPagination"
 import { CustomBreadCrumbs } from "@/components/custom/CustomBreadCrumbs"
-import { getHeroesByPage } from "@/heroes/actions/get-heroes-by-page.action"
+import { getHeroesByPageAction } from "@/heroes/actions/get-heroes-by-page.action"
+import { useQuery } from "@tanstack/react-query"
 
 export const HomePage = () => {
 
   const [activeTab, setActiveTab] = useState<'all' | 'favorites' | 'heroes' | 'villains'>('all')
 
-  useEffect(() => {
-    getHeroesByPage().then((heroes) => {
-      console.log({heroes})
-    })
-  },[])
+  const { data } = useQuery({
+    queryKey: ['heroes'],
+    queryFn: () => getHeroesByPageAction(),
+    staleTime: 1000 * 60 * 5 
+  })
+  // useEffect(() => {
+  //   getHeroesByPageAction().then()
+  // },[])
+
   return (
     <>
       <>
